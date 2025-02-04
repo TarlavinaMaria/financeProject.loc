de38822ee861f7d549a96da3a1691ff8c0bde00a
Мария Тарлавина <tarlavina50@mail.ru>
2025-02-04 23:18:07 +0700

    Введение +

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -20,14 +20,17 @@
                     </button>
                     <div class=" collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="diary">Мой дневник</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="diary">Главная</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Учет доходов</a>
+                                <a class="nav-link" href="#">Мой дневник</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Учет расходов</a>
+                                <a class="nav-link" href="#">Мои заметки</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Кошелек</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="logout">Выход</a>
