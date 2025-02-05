87e428cbdc82748e072b08ff3c0b12af1fb97151
Мария Тарлавина <tarlavina50@mail.ru>
2025-02-05 17:38:47 +0700

    Добавление станицы расходы

---
+++ app/controllers/add_expense.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+// Извлечение категорий из базы данных
+$result = $db->query("SELECT CategoryName FROM Category WHERE TypeCategory = 0");
+$categories = $result->findAll();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получаем данные из формы
+    $sum = htmlspecialchars(trim($_POST['sum']));
+    $sum = -$sum;
+    $category_name = htmlspecialchars(trim($_POST['category']));
+    $date = htmlspecialchars(trim($_POST['date']));
+    $comment = htmlspecialchars(trim($_POST['comment']));
+
+    // Получаем ID категории
+    $category = $db->query("SELECT Category_id FROM Category WHERE CategoryName = ?", [$category_name])->find();
+
+    if ($category) {
+        $category_id = $category['Category_id'];
+
+        // Сохранение данных в базу данных с использованием подготовленного выражения
+        $stmt = $db->query("INSERT INTO Action (Category, Sum, Date, Comment, User_id) VALUES (?, ?, ?, ?, ?)", [$category_id, $sum, $date, $comment, $user_id]);
+
+        // Проверка успешности выполнения запроса
+        if ($stmt) {
+
+            header('Location: wallet');
+            exit;
+        } else {
+            echo "Ошибка при добавлении дохода.";
+        }
+    } else {
+        echo "Категория не найдена.";
+    }
+}
+
+require_once(VIEWS . '/add_income.tmpl.php');
+?>
---
+++ app/views/add_expense.tmpl.php
@@ -0,0 +1,38 @@
+<?php require(COMPONENTS . "/header.php"); ?>
+<main class="main py-3">
+    <div class="container">
+        <h1>Добавить Расход</h1>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="sum">Сумма (₽):</label>
+                <input type="number" id="sum" name="sum" required class="form-control" step="1" min="0">
+            </div>
+
+            <div class="form-group">
+                <label for="category">Категория:</label>
+                <select id="category" name="category" required class="form-control">
+                    <option value="">Выберите категорию</option>
+                    <?php foreach ($categories as $category): ?>
+                        <option value="<?php echo htmlspecialchars($category['CategoryName']); ?>">
+                            <?php echo htmlspecialchars($category['CategoryName']); ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="date">Дата:</label>
+                <input type="date" id="date" name="date" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="comment">Комментарии:</label>
+                <textarea id="comment" name="comment" class="form-control" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Добавить расход</button>
+        </form>
+    </div>
+</main>
+
+<?php require(COMPONENTS . "/footer.php"); ?>
